ca47d732b23de7631c29c112fc85f8096e29b1dd
Kenneth Skelton <kskelto1@stevens.edu>
2023-04-29 13:30:23 -0400

    Initial Commit

---
+++ TournamentApp/TournamentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TournamentAppApp.swift
+//  TournamentApp
+//
+//  Created by Kenneth Skelton on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TournamentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
